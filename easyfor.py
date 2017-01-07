ed8e24076911dbf825e7d78f72451f5dc71be332
JYLiu611 <j.y_liu@hotmail.com>
2017-01-07 16:31:15 +0800

    xiugai

--- easyfor.py
+++ easyfor.py
@@ -1,7 +1,9 @@
-largest=-1
+largest=None
 number=0
 for num in [9,41,12,3,74,15]:
     number=number+1
-    if num>largest:
+    if largest is None:
+        largest=num
+    elif largest<num:
         largest=num
 print "the largest is",largest,";","\n","It`s number is",number
