ad91c598296dc1cfeb6fcfb43026252b4fa0479c
JYLiu611 <j.y_liu@hotmail.com>
2017-01-07 17:37:10 +0800

    L

--- tryexcept.py
+++ tryexcept.py
@@ -1,12 +1,22 @@
-hrs = raw_input("Enter Hours:")
-rate = raw_input("Enter rate per Hours:")
-try:
-    h = float(hrs)
-    rate = float(rate)
-    if h>=40:
-        m=40*rate+(h-40)*1.5*rate
-    else:
-        m=h*rate
-    print m
-except:
-    print"Something wrong happened."
+largest = None
+smallest = None
+while True:
+    num = raw_input("Enter a number: ")
+    try:
+        num=int(num)
+    except:
+        if num == "done" :
+            print "Invalid input"
+            break    
+    if largest is None:
+            largest=num
+            smallest=num
+    elif largest<num:
+            largest=num
+    elif smallest>num:
+            smallest=num
+
+
+
+print "Maximum is", largest
+print "Minimum is",smallest   
