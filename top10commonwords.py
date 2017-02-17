13c0f24bb337d796145cd84da9f7d0a41cf2549a
JYLiu611 <j.y_liu@hotmail.com>
2017-02-17 17:13:41 +0800

    top 10 words

---
+++ top10commonwords.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-  
+fileopen=open("commonwords.txt")
+count=dict()
+for line in fileopen:
+    words=line.split()
+    for word in words:
+        count[word]=count.get(word,0)+1
+lst=list()
+for a,b in count.items():
+    lst.append((b,a))
+        
+        
+lst.sort(reverse=True)
+for number,name in lst[:10]:
+    print name,number
